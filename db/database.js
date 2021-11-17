48651437b56918a0acca4f1e4667afb39b9a38c3
hanna <hanna.aspblad@iths.se>
2021-11-17 10:17:11 +0100

    added in memory db

---
+++ db/database.js
@@ -0,0 +1,55 @@
+const database = [
+  {
+    users: [
+      {
+        name: "Kalle",
+        login: 403274618461,
+      },
+      {
+        name: "Lotta",
+        login: 413274618461,
+      },
+      {
+        name: "Pluto",
+        login: 423274618461,
+      },
+    ],
+    carts: [
+      {
+        403274618461: [
+          {
+            productId: 152164826489,
+            amount: 2,
+          },
+          {
+            productId: 752164826789,
+            amount: 1,
+          },
+        ],
+        413274618461: [
+          {
+            productId: 167164826489,
+            amount: 1,
+          },
+        ],
+      },
+    ],
+    products: [
+      {
+        id: 152164826489,
+        name: "vegankorv",
+        price: 30,
+      },
+      {
+        id: 167164826489,
+        name: "förkläde",
+        price: 349,
+      },
+      {
+        id: 752164826789,
+        name: "senap",
+        price: 22,
+      },
+    ],
+  },
+];
