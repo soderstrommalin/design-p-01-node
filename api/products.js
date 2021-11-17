5c909fa57ea1693d73914962b487d7fc6539d16a
Nitipon Apaisri <nitipon.apaisri@gmail.com>
2021-11-17 11:49:19 +0100

    Add example for import database

--- api/products.js
+++ api/products.js
@@ -1,5 +1,8 @@
+const { database } = require("../db/database.js");
+
 const getAllProducts = async (req, res, next) => {
     try {
+        console.log(database[0].products);
         res.json({ message: "hallå alla produkter hehe" }).status(200); //skriv model för products
     } catch (err) {
         next(err);
