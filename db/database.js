97a023d069bd355308a08b02c9764ac0c898d01e
Malin S <malin.soderstrom@iths.se>
2021-11-18 09:59:53 +0100

    changed ids to string

--- db/database.js
+++ db/database.js
@@ -3,32 +3,32 @@ const database = [
         users: [
             {
                 name: "Kalle",
-                login: 403274618461,
+                login: "403274618461",
             },
             {
                 name: "Lotta",
-                login: 413274618461,
+                login: "413274618461",
             },
             {
                 name: "Pluto",
-                login: 423274618461,
+                login: "423274618461",
             },
         ],
         carts: [
             {
-                403274618461: [
+                "403274618461": [
                     {
-                        productId: 152164826489,
+                        productId: "152164826489",
                         amount: 2,
                     },
                     {
-                        productId: 752164826789,
+                        productId: "752164826789",
                         amount: 1,
                     },
                 ],
-                413274618461: [
+                "413274618461": [
                     {
-                        productId: 167164826489,
+                        productId: "167164826489",
                         amount: 1,
                     },
                 ],
@@ -36,17 +36,17 @@ const database = [
         ],
         products: [
             {
-                id: 152164826489,
+                id: "152164826489",
                 name: "vegankorv",
                 price: 30,
             },
             {
-                id: 167164826489,
+                id: "167164826489",
                 name: "förkläde",
                 price: 349,
             },
             {
-                id: 752164826789,
+                id: "752164826789",
                 name: "senap",
                 price: 22,
             },
