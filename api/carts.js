bd492e4739239ca6f3583e8facda757246bed2d7
Nitipon Apaisri <nitipon.apaisri@gmail.com>
2021-11-25 11:32:13 +0100

    Added test for delete item in cart

--- api/carts.js
+++ api/carts.js
@@ -1,58 +1,68 @@
-const { InvalidCredentials } = require("../errors/errors")
-const cartsModel = require("../models/carts")
-const { getProductById } = require("../models/products")
-const { getUserById } = require("../models/users")
+const { InvalidCredentials } = require("../errors/errors");
+const cartsModel = require("../models/carts");
+const { getProductById } = require("../models/products");
+const { getUserById } = require("../models/users");
 
 const getUserCart = (req, res, next) => {
-  const { userLogin } = req.params
-  try {
-    getUserById(userLogin)
-    const cart = cartsModel.getUserCart(userLogin)
-    res.json({ data: cart })
-  } catch (err) {
-    next(err)
-  }
-}
+    const { userLogin } = req.params;
+    try {
+        getUserById(userLogin);
+        const cart = cartsModel.getUserCart(userLogin);
+        res.json({ data: cart });
+    } catch (err) {
+        next(err);
+    }
+};
 
 const addToUserCart = async (req, res, next) => {
-  const { userLogin } = req.params
-  const cart = req.body.cart
-  try {
-    cart.forEach((product) => {
-      if (product.productId == undefined || product.amount == undefined) {
-        throw new InvalidCredentials()
-      }
-      getProductById(product.productId)
-    })
-    cartsModel.addToUserCart(userLogin, cart)
-    res.json({ msg: `Cart created for ${userLogin}` })
-  } catch (err) {
-    next(err)
-  }
-}
+    const { userLogin } = req.params;
+    const cart = req.body.cart;
+    try {
+        cart.forEach((product) => {
+            if (product.productId == undefined || product.amount == undefined) {
+                throw new InvalidCredentials();
+            }
+            getProductById(product.productId);
+        });
+        cartsModel.addToUserCart(userLogin, cart);
+        res.json({ msg: `Cart created for ${userLogin}` });
+    } catch (err) {
+        next(err);
+    }
+};
 
 const editItemInCart = async (req, res, next) => {
-  const { userLogin, productId } = req.params
-  const { amount } = req.body
+    const { userLogin, productId } = req.params;
+    const { amount } = req.body;
 
-  try {
-    if (typeof amount !== "number") {
-      throw new InvalidCredentials()
+    try {
+        if (typeof amount !== "number") {
+            throw new InvalidCredentials();
+        }
+
+        cartsModel.editItemInCart(userLogin, productId, amount);
+
+        cartsModel.getProductById(productId);
+    } catch (err) {
+        next(err);
     }
+};
 
-    cartsModel.editItemInCart(userLogin, productId, amount)
-
-    cartsModel.getProductById(productId)
-  } catch (err) {
-    next(err)
-  }
-}
-
-const deleteItemInCart = async (req, res, next) => {}
+const deleteItemInCart = async (req, res, next) => {
+    const { userLogin, itemId } = req.params;
+    try {
+        getUserById(userLogin);
+        getProductById(itemId);
+        cartsModel.deleteItemInCart(userLogin, itemId);
+        res.json({ msg: "Item deleted" });
+    } catch (err) {
+        next(err);
+    }
+};
 
 module.exports = {
-  getUserCart,
-  addToUserCart,
-  editItemInCart,
-  deleteItemInCart,
-}
+    getUserCart,
+    addToUserCart,
+    editItemInCart,
+    deleteItemInCart,
+};
--- models/carts.js
+++ models/carts.js
@@ -1,34 +1,38 @@
-const { database } = require("../db/database.js")
-const { NoExistingCart } = require("../errors/errors.js")
+const { database } = require("../db/database.js");
+const { NoExistingCart } = require("../errors/errors.js");
 
-const cartArr = database[0].carts
+const cartArr = database[0].carts;
 const getUserCart = (id) => {
-  try {
-    const cartIndex = cartArr.findIndex((r) => r.login == id)
-    if (cartIndex === -1) throw new NoExistingCart()
-    const cart = cartArr[cartIndex].cart
-    return cart
-  } catch (err) {
-    throw err
-  }
-}
+    try {
+        const cartIndex = cartArr.findIndex((r) => r.login == id);
+        if (cartIndex === -1) throw new NoExistingCart();
+        const cart = cartArr[cartIndex].cart;
+        return cart;
+    } catch (err) {
+        throw err;
+    }
+};
 
 const addToUserCart = (login, cart) => {
-  if (cartArr.find((cart) => cart.login == login)) {
-    throw Error
-  }
-  console.log({ login: login, cart })
-  cartArr.push({ login: login, cart })
-}
+    if (cartArr.find((cart) => cart.login == login)) {
+        throw Error;
+    }
+    console.log({ login: login, cart });
+    cartArr.push({ login: login, cart });
+};
 
 const editItemInCart = () => {
-  throw Error
-}
+    throw Error;
+};
 
-const deleteItemInCart = (id) => {}
+const deleteItemInCart = (userLogin, itemId) => {
+    const userCart = getUserCart(userLogin);
+    const itemIndex = userCart.findIndex((item) => item.productId == itemId);
+    userCart.splice(itemIndex, 1);
+};
 module.exports = {
-  getUserCart,
-  addToUserCart,
-  editItemInCart,
-  deleteItemInCart,
-}
+    getUserCart,
+    addToUserCart,
+    editItemInCart,
+    deleteItemInCart,
+};
