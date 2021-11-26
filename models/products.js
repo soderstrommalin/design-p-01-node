debf8be89f71e80c231c201fecaf3099f82b5ad1
hanna <hanna.aspblad@iths.se>
2021-11-26 13:11:28 +0100

    fixed edit product and post product when deleting utils file

--- models/products.js
+++ models/products.js
@@ -1,44 +1,44 @@
-const { database } = require("../db/database.js");
-const { v4: uuidv4 } = require("uuid");
-const { NoExsistingProduct } = require("../errors/errors.js");
-const allProducts = database[0].products;
+const { database } = require("../db/database.js")
+const { v4: uuidv4 } = require("uuid")
+const { NoExsistingProduct } = require("../errors/errors.js")
+const allProducts = database[0].products
 
 const getAllProducts = () => {
-    return allProducts;
-};
+  return allProducts
+}
 
 const getProductById = (id) => {
-    const product = allProducts.find((prod) => prod.id == id);
-    if (product == undefined) throw new NoExsistingProduct();
-    return product;
-};
+  const product = allProducts.find((prod) => prod.id == id)
+  if (product == undefined) throw new NoExsistingProduct()
+  return product
+}
 
 const postProduct = (name, price) => {
-    const product = {
-        id: uuidv4(),
-        name: name,
-        price: price,
-    };
-    allProducts.push(product);
-};
+  const product = {
+    id: uuidv4(),
+    name: name,
+    price: price,
+  }
+  allProducts.push(product)
+}
 
 const editProductById = (id, name, price) => {
-    const productIndex = database[0].products.findIndex((prod) => prod.id == id);
-    if (productIndex == -1) throw new NoExsistingProduct();
-    if (name !== undefined) database[0].products[productIndex].name = name;
-    if (price !== undefined) database[0].products[productIndex].price = price;
-};
+  const productIndex = allProducts.findIndex((prod) => prod.id == id)
+  if (productIndex == -1) throw new NoExsistingProduct()
+  if (name !== undefined) allProducts[productIndex].name = name
+  if (price !== undefined) allProducts[productIndex].price = price
+}
 
 const deleteProductById = (id) => {
-    const productIndex = allProducts.findIndex((prod) => prod.id == id);
-    if (productIndex == -1) throw new NoExsistingProduct();
-    allProducts.splice(productIndex, 1);
-};
+  const productIndex = allProducts.findIndex((prod) => prod.id == id)
+  if (productIndex == -1) throw new NoExsistingProduct()
+  allProducts.splice(productIndex, 1)
+}
 
 module.exports = {
-    getAllProducts,
-    getProductById,
-    postProduct,
-    editProductById,
-    deleteProductById,
-};
+  getAllProducts,
+  getProductById,
+  postProduct,
+  editProductById,
+  deleteProductById,
+}
--- utils.js
+++
@@ -1,8 +0,0 @@
-const checkDataType = (name, price) => {
-  if (typeof name !== "string" || typeof price !== "number") {
-    return false
-  }
-  return true
-}
-
-module.exports = { checkDataType }
